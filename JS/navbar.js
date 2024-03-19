6d98a9d16d7640dde9e57fe960b2a6223682dfc8
DCKAP <vijayalakshmigdckap@gmail.com>
2024-03-19 14:53:45 +0530

    issue fixed in js

---
+++ JS/navbar.js
@@ -0,0 +1,17 @@
+//-------------------------- login signup process --------------------------
+
+document.addEventListener("DOMContentLoaded", function() {
+    let admintag = document.querySelector('.menu li:nth-child(5)'); 
+  
+   
+  
+    if (loginDetailsExists ) {
+      
+      
+    } else {
+    
+  
+    }
+});
+let userData =JSON.parse(sessionStorage.getItem('login_details'))
+console.log(userData)
