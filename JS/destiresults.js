2cf728f418543e215c8f6120048f39d4ae4a040d
Akilashwarran <akilashwarranpdckap@gmail.com>
2024-03-20 15:52:01 +0530

    issues-created

--- JS/destiresults.js
+++ JS/destiresults.js
@@ -102,19 +102,21 @@ function initMap() {
     }
         
     var places = selectedPackage.places;
-    var origin = places[0].name + ', Tamil Nadu, India';
-    var destination = places[places.length - 1].name + ', Tamil Nadu, India';
+    var origin = places[places.length - 1].name + ', Tamil Nadu, India'; 
+    var destination = places[0].name + ', Tamil Nadu, India'; 
     var waypoints = places.slice(1, -1).map(place => ({ location: place.name + ', Tamil Nadu, India', stopover: true }));
 
-    var map = new google.maps.Map(document.getElementById('pic'));
+    var map = new google.maps.Map(document.getElementById('pic'), {
+        
+    });
 
     var directionsService = new google.maps.DirectionsService();
     var directionsRenderer = new google.maps.DirectionsRenderer();
     directionsRenderer.setMap(map);
 
     var request = {
-        origin: origin,
-        destination: destination,
+        origin: origin, 
+        destination: destination, 
         waypoints: waypoints,
         travelMode: google.maps.TravelMode.DRIVING,
     };
